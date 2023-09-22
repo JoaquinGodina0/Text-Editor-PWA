fdbfde53faa8501b8c666f08e7e5f8bcc642c3d5
JoaquinGodina0 <godinajoaquin6@gmail.com>
2023-09-22 12:59:49 -0700

    redid code from starter code and reinstalled and fixed code

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,20 +1,20 @@
 // Import methods to save and get data from the indexedDB database in './database.js'
-import { getDb, putDb } from './database';
-import { header } from './header';
+import { initdb, getDb, putDb } from "./database";
+import { header } from "./header";
 
 export default class {
   constructor() {
-    const localData = localStorage.getItem('content');
+    const localData = localStorage.getItem("content");
 
     // check if CodeMirror is loaded
-    if (typeof CodeMirror === 'undefined') {
-      throw new Error('CodeMirror is not loaded');
+    if (typeof CodeMirror === "undefined") {
+      throw new Error("CodeMirror is not loaded");
     }
 
-    this.editor = CodeMirror(document.querySelector('#main'), {
-      value: '',
-      mode: 'javascript',
-      theme: 'monokai',
+    this.editor = CodeMirror(document.querySelector("#main"), {
+      value: "",
+      mode: "javascript",
+      theme: "monokai",
       lineNumbers: true,
       lineWrapping: true,
       autofocus: true,
@@ -24,19 +24,40 @@ export default class {
 
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+    initdb();
+
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      console.info("Loaded data from IndexedDB, injecting into editor");
+
+      if (data && data.length) {
+        this.editor.setValue(header.concat(data.pop().content));
+      } else if (localData) {
+        this.editor.setValue(header.concat(localData));
+      } else {
+        this.editor.setValue(header);
+      }
     });
 
-    this.editor.on('change', () => {
-      localStorage.setItem('content', this.editor.getValue());
+    this.editor.on("change", () => {
+      localStorage.setItem(
+        "content",
+        this.editor.getValue().slice(header.length)
+      );
     });
 
     // Save the content of the editor when the editor itself is loses focus
-    this.editor.on('blur', () => {
-      console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content'));
+    this.editor.on("blur", async () => {
+      console.log("The editor has lost focus");
+      const data = await getDb();
+
+      if (data && data.length) {
+        const latest = data.pop().content;
+        if (localStorage.getItem("content") !== latest) {
+          putDb(localStorage.getItem("content"));
+        }
+      } else {
+        putDb(localStorage.getItem("content"));
+      }
     });
   }
 }
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,11 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
-
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Configure workbox plugins for a service worker and manifest file.
-const WorkboxPlugin = require("workbox-webpack-plugin");
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = () => {
   return {
@@ -20,10 +18,13 @@ module.exports = () => {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
+    devServer: {
+      hot: "only",
+    },
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "Webpack Plugin",
+        title: "JATE",
       }),
       new MiniCssExtractPlugin(),
       new InjectManifest({
@@ -33,13 +34,13 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Just Another Text Editor",
-        short_name: "J.A.T.E",
-        description: "offline text editor",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
-        start_url: "/",
-        publicPath: "/",
+        name: "JATE",
+        short_name: "JATE",
+        description: "Just another text editor",
+        background_color: "#7eb4e2",
+        theme_color: "#7eb4e2",
+        start_url: "./",
+        publicPath: "./",
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
@@ -49,19 +50,20 @@ module.exports = () => {
         ],
       }),
     ],
+
     module: {
       rules: [
         {
-          test: /\.css$/i,
+          test: /\.css$/i, //css files
           use: ["style-loader", "css-loader"],
         },
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          test: /\.(png|svg|jpg|jpeg|gif)$/i, //image files
           type: "asset/resource",
         },
         {
           test: /\.m?js$/,
-          exclude: /node_modules/,
+          exclude: /(node_modules|bower_components)/,
           use: {
             loader: "babel-loader",
             options: {
